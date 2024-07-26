732db6837a9ad3ce7b123aaceaf23f7971235372
Andrew Gelstein <gelstein.andrew@gmail.com>
2024-07-26 15:45:28 -0500

    added a reactive form to submit a name to the hero search service

--- src/app/components/hero-search/hero-search.component.ts
+++ src/app/components/hero-search/hero-search.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { SearchNameService } from '../../api/search-hero-name.service';
 
 @Component({
   selector: 'app-hero-search',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './hero-search.component.html',
-  styleUrl: './hero-search.component.css'
+  styleUrl: './hero-search.component.css',
 })
 export class HeroSearchComponent {
+  searchForm: FormGroup;
 
+  constructor(
+    private fb: FormBuilder,
+    private searchNameService: SearchNameService
+  ) {
+    this.searchForm = this.fb.group({
+      query: [''],
+    });
+  }
+
+  onSubmit() {
+    if (this.searchForm.valid) {
+      const queryValue = this.searchForm.get('query')?.value;
+      this.searchNameService
+        .getHeroByName(queryValue)
+        // leaky subscription
+        .subscribe((response) => {
+          console.log('Response:', response);
+        });
+    }
+  }
 }
