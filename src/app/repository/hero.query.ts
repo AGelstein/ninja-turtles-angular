2c3bc723d8adadf606e06986ca9bc92fd437fccd
Andrew Gelstein <gelstein.andrew@gmail.com>
2024-08-03 14:40:59 -0500

    remove dead code

--- src/app/repository/hero.query.ts
+++
@@ -1,5 +0,0 @@
-import { selectAllEntities, selectEntitiesCount } from '@ngneat/elf-entities';
-import { HeroStore } from './hero.store';
-
-export const SelectAllHeroQuery = HeroStore.pipe(selectAllEntities());
-export const SelectHeroCountQuery = HeroStore.pipe(selectEntitiesCount());
--- src/app/repository/hero.store.ts
+++
@@ -1,5 +0,0 @@
-import { createStore } from '@ngneat/elf';
-import { withEntities } from '@ngneat/elf-entities';
-import { Hero } from '../models/Hero';
-
-export const HeroStore = createStore({ name: 'hero' }, withEntities<Hero>());
