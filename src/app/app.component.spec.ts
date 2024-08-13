5c59959902802feb00c065e574b24339c18f47ae
Andrew Gelstein <gelstein.andrew@gmail.com>
2024-08-13 16:01:50 -0500

    clean up the testing

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,12 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'ninja-turtles-app' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('ninja-turtles-app');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ninja-turtles-app');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Hello, ninja-turtles-app'
+    );
   });
 });
