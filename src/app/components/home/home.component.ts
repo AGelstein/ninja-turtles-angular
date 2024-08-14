5cf863b5aec3c9bd38bc9c980903c90a294e4a43
Andrew Gelstein <gelstein.andrew@gmail.com>
2024-08-14 17:21:50 -0500

    Initial audit log design set in

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,11 +3,18 @@ import { HeroSearchComponent } from '../hero-search/hero-search.component';
 import { HeroCardComponent } from '../hero-card/hero-card/hero-card.component';
 import { HeroRepository } from '../../repository/hero.repository';
 import { AsyncPipe, NgFor } from '@angular/common';
+import { AuditLogComponent } from '../action-log/audit-log/audit-log.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeroSearchComponent, HeroCardComponent, AsyncPipe, NgFor],
+  imports: [
+    HeroSearchComponent,
+    HeroCardComponent,
+    AsyncPipe,
+    NgFor,
+    AuditLogComponent,
+  ],
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
