4a65206e4e03e46d8d52ba9568badfbe2a96210d
AGelstein <gelstein.andrew@gmail.com>
2024-09-07 13:57:58 -0500

    Setting up data to be queried against. I'm going to need some way to tell my local dev to point at postgres instance.

--- dbscripts/0-init.sql
+++ dbscripts/0-init.sql
@@ -1,11 +1,17 @@
 CREATE ROLE hank WITH LOGIN SUPERUSER PASSWORD 'hank';
-CREATE TABLE inflation_data (
-    RegionalMember TEXT,
-    Year INT,
-    Inflation DECIMAL,
-    Unit_of_Measurement TEXT,
-    Subregion TEXT,
-    Country_Code TEXT
+CREATE TABLE superheroes (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    hero_name VARCHAR(50) NOT NULL,
+    hero_power_level INT NOT NULL,
+    is_human BOOLEAN NOT NULL,
+    number_of_missions INT,
+    hero_age INT,
+    origin VARCHAR(50)
 );
-INSERT INTO inflation_data (RegionalMember, Year, Inflation, Unit_of_Measurement, Subregion, Country_Code)
-VALUES ('United States', 2023, 3.4, 'percent', 'North America', 'US');
+INSERT INTO superheroes (hero_name, hero_power_level, is_human, number_of_missions, hero_age, origin)
+VALUES 
+    ('Captain Velocity', 90, TRUE, 150, 32, 'Metro City'),
+    ('Mighty Boulder', 85, FALSE, 200, 40, 'Rockridge Mountains'),
+    ('Techno Phantom', 78, TRUE, 120, 28, 'Cyber Haven'),
+    ('Aqua Blaze', 88, FALSE, 100, 25, 'Oceanis'),
+    ('Night Falcon', 82, TRUE, 175, 35, 'Skyline City');
