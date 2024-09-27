c45f7eae58502c75727c02ca38bbe616ab3f204e
AGelstein <gelstein.andrew@gmail.com>
2024-09-27 15:40:47 -0500

    reverse audit log so newest is top

--- src/app/components/action-log/audit-log/audit-log.component.ts
+++ src/app/components/action-log/audit-log/audit-log.component.ts
@@ -3,6 +3,7 @@ import { AuditLogRepository } from '../../../repository/audit-log.repository';
 import { AsyncPipe } from '@angular/common';
 import { Observable } from 'rxjs';
 import { AuditLog } from '../../../models/AuditLog';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-audit-log',
@@ -15,7 +16,10 @@ export class AuditLogComponent {
   private readonly auditLogRepository = inject(AuditLogRepository)
   logsExist$: Observable<boolean> = this.auditLogRepository.logsExist$
   auditLog$: Observable<AuditLog[]> = this.auditLogRepository.selectAllAuditLogs()
+  reversedAuditLog$: Observable<AuditLog[]> = this.auditLog$.pipe(
+    filter((logs): logs is AuditLog[] => logs !== undefined),
+    map((logs: AuditLog[]) => logs.slice().reverse())
+  );
 }
-  // TODO the newest log should appear at the top of the list rather than the bottom
+
   // TODO Should have a limitation of how many rows we can show before a paging behavior occurs
-  // TODO need title for "Audit Log" in component
