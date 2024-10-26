7f6491811c008b81270c96f9b7e943778b961d9f
AGelstein <gelstein.andrew@gmail.com>
2024-10-26 09:51:23 -0500

    added signals component on different route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,17 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { SignalsComponent } from './components/signals/signals.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'signals',
+    component: SignalsComponent,
     pathMatch: 'full',
   },
   {
---
+++ src/app/components/signals/signals.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-signals',
+  standalone: true,
+  imports: [],
+  templateUrl: './signals.component.html',
+  styleUrl: './signals.component.css'
+})
+export class SignalsComponent {
+
+}
