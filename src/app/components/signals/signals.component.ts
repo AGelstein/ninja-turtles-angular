c4585ee7d17930d3bd591a8a0d5b37bcdabc7fb0
AGelstein <gelstein.andrew@gmail.com>
2024-10-28 15:13:06 -0500

    push changes

--- src/app/components/signals/signals.component.ts
+++ src/app/components/signals/signals.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, OnInit, signal, Signal, WritableSignal } from '@angular/core';
 
 @Component({
   selector: 'app-signals',
@@ -7,6 +7,26 @@ import { Component } from '@angular/core';
   templateUrl: './signals.component.html',
   styleUrl: './signals.component.css'
 })
-export class SignalsComponent {
+export class SignalsComponent implements OnInit {
 
+  count: WritableSignal<number> = signal(0);
+  doubleCount: Signal<number> = computed(() =>  this.count() * 2);
+
+ngOnInit(): void {
+  console.log('count: ', this.count());
+  console.log('double count: ', this.doubleCount());
+ }
+
+increment(): void {
+  console.log('increment: ', this.count());
+  this.count.update(value => value + 1);
+}
+
+decrement(): void {
+  this.count.update(value => value - 1);
+}
+
+reset(): void {
+  this.count.set(0);
+}
 }
